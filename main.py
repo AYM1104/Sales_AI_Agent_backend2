40f55edd7967c433ed15a7d3e372c3b239fd800f
ak3668134 <ak3668134@gmail.com>
2025-07-24 12:57:17 +0900

    no message

--- main.py
+++ main.py
@@ -124,5 +124,4 @@ IT・デジタル投資：システム関連投資の記載
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
