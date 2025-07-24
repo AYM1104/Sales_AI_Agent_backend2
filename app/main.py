08c6fa51791aa0f69bbfbb32cdf56ee8fe39dba2
ak3668134 <ak3668134@gmail.com>
2025-07-24 12:42:29 +0900

    appフォルダに移動

--- app/main.py
+++ app/main.py
@@ -124,4 +124,4 @@ IT・デジタル投資：システム関連投資の記載
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000)
